5229d01883bf735388dbc8cbb05340af3a08b639
EMEX <emex4gman@gmail.com>
2019-07-11 19:01:02 +0300

    this is just a test project, revamped a lot and you can now update the any username if you logged in,will change that in my next commit

--- app.js
+++ app.js
@@ -37,15 +37,17 @@ passport.use(new localStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+        //////////////////////////////////////
+       //              ROUTES              //
+      //////////////////////////////////////
 
+//HOMEPAGE ROUTE      
 app.get("/", function(req, res){
         res.render("landing");
     }); 
 
-
-
 // index route 
-app.get("/emex", function(req, res){
+app.get("/emex",isLoggedIn, function(req, res){
     User.find({}, function(err, users){
         if(err){
             console.log(err);
@@ -55,98 +57,93 @@ app.get("/emex", function(req, res){
     });    
 });
 
-
 // show route will show a users secrst page containing thire username and an update form
-app.get("/emex/:id", function(req, res){
-    res.render("secret");
+app.get("/emex/:id", isLoggedIn, function(req, res){
+    User.findById(req.params.id, function(err, foundUser){
+        res.render("secret",{user: foundUser});
+    });
 });
 
-//Auth routs
-//login
+
+
+                    //////////////////////////////////////
+                   //         AUTH ROUTES              //
+                  //////////////////////////////////////
+//login FORM
+app.get("/login" ,function(req, res){
+    res.render("login");
+})
+
 app.post("/login", passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/emex"
-}) , function(req, res){
+        successRedirect: "/emex",
+        failureRedirect: "/"
+    }) , function(req, res){
     
 });
 
 //SIGN UP
-// create route 
-// app.post("/emex", function(req, res){
-//     var username = req.body.username;
-//     var password =  req.body.password;
-//     var newUser = { username: username, password: password};
+app.get("/signup" ,function(req, res){
+    res.render("signup");
+})
 
-//     User.create(newUser, function(err, newlyUser){
-//         if(err){
-//             console.log(err);
-//         } else{
-//             // newlyUser.save();
-//             res.redirect("/emex")
-//         }
-//     });
-    
-// });
-app.post("/emex", function(req, res){
+app.post("/signup", function(req, res){
     req.body.username; // gets the username and password that was inputed
     req.body.password;
     //registers the details and turns the password into hash code and also saves into database
     User.register(new User({username: req.body.username}), req.body.password, function(err, user){
         if(err){
             console.log(err);
-            // res.render("index"); 
+            res.redirect("/signup");
         }
         // the this code below use the local option which can be changed to any other authtication method like google, facebook or even twitter
         passport.authenticate("local")(req, res, function(){
-            res.redirect("/");
+            res.redirect("/emex"); 
         });
     });
 });
 
-
 //LOG OUT ROUTE
 app.get("/logout", function(req, res){
     req.logout();
-    res.redirect("/");
+    res.redirect("/"); 
     console.log("you have logged out");
 });
-
-// new route
-app.get("/emex/signup", function(req, res){
-    res.redirect("/emex");
-});
-
 
 
 // edit route
 app.get("/emex/:id/edit", function(req, res){
-    res.send("edit route");
+    res.redirect("/emex");
 });
 
 // update route
-app.put("/emex/:id", function(req, res){
-    res.send("update route");
+app.put("/emex/:id", isLoggedIn, function(req, res){
+    User.findByIdAndUpdate(req.params.id, req.body.user, function(err, updatedUsername){
+        if(err){
+            res.redirect("/");
+        }else{
+            res.redirect("/emex")
+        }
+    })
+    res.redirect("/emex");
 });
 
 // destroy route
-app.delete("/emex", function(req, res){
-    res.send("update route");
-});
+// app.delete("/emex", function(req, res){
+//     res.send("update route");
+// });
 
-app.get("/secret", function(req, res){
-    res.render("secret");
-})
+
 
 
 
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
-        return next;
+        return next();
     }
-    res.redirect("/login")
+    res.redirect("/")
+    console.log("signup or login to gain access")
 }
 
 app.listen(3000, function(){
     console.log("API DON START");
 })
-
